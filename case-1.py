f6b0dbd3086057685bd2fd1304959c152e912305
Avvacuum <76999612+Avvacuum@users.noreply.github.com>
2021-01-05 22:56:54 +0200

    Add files via upload

---
+++ case-1.py
@@ -0,0 +1,18 @@
+"""
+    TODO: 
+        input_line get integers divided by w_spaces
+        print int_sum 
+        ie.: 2 -1 9 6 = 16
+    TODO:
+        write 2 function's: get user input & print calculated sum
+"""
+
+# list of char's
+a = int(input("a = "))
+b = int(input("b = "))
+c = int(input("c = "))
+d = int(input("d = "))
+
+# operation
+sum = a + b + c + d
+print("Sum = " + str(sum))
