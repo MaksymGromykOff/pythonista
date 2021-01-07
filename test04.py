e8bf4644e78fa8a779f451686023cb339ef6f662
Avvacuum <76999612+Avvacuum@users.noreply.github.com>
2021-01-07 23:21:52 +0200

    Add files via upload

---
+++ test04.py
@@ -0,0 +1,23 @@
+# TODO: dictionary with values
+inventory = {
+  'gold': 500,
+  'pouch': ['flint', 'twine', 'gemstone'],
+  'backpack': ['xylophone', 'dagger', 'bedroll', 'breadloaf']
+}
+
+# TODO: add key_pocket and values to dictionary
+inventory["pocket"] = ['seashell', 'strange berry', 'lint']
+
+# TODO: ( .sort()) elements in backpack and print
+#inventory.sort() #error
+
+#print(inventory)
+
+# TODO: del dagger from list
+#inventory['dagger'].remove() #KeyError: 'dagger'
+#del inventory[2:1] #TypeError: unhashable type: 'slice'
+# &nihyja_ne_pahae.exe
+
+# TODO: add 50 gold and print results
+
+print(inventory)
