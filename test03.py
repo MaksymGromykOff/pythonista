c5dbb86a8bf141364406096c31147213ed617c29
Avvacuum <76999612+Avvacuum@users.noreply.github.com>
2021-01-07 22:39:36 +0200

    Add files via upload

---
+++ test03.py
@@ -0,0 +1,19 @@
+
+# english-german dictionary wordlist
+e2g = {
+  'stork': 'storch', 
+  'hawk': 'falke', 
+  'woodpecker': 'specht', 
+  'owl': 'eule'
+}
+
+# print translation for owl
+print(e2g["owl"])
+
+# add 2 words and translation
+e2g["tree"]= "baum"
+e2g["river"]= "fluss"
+
+# print words and translation
+for key, value in e2g.items():
+  print (key, " : ", value)
